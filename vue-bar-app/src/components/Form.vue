79beefc1653ac8016a964e9166347011a0e393ce
Rafah <53227198+rafahalani@users.noreply.github.com>
2020-08-17 11:30:19 +0200

    errors 've been fixed

--- vue-bar-app/src/components/Form.vue
+++ vue-bar-app/src/components/Form.vue
@@ -1,24 +1,25 @@
 <template>
     <div>
-        <form v-form v-on:submit="sub" @submit.prevent="checkForm">
+        <form @submit.prevent="checkForm">
             <section>
                 <h5></h5>
-                <input v-model="name" type="text" placeholder="Please fill in your name?" class="form-control form-control-lg">
+                <input v-model="form.name" type="text" placeholder="Please fill in your name?" class="form-control form-control-lg">
             </section>
             <section>
                 <h5></h5>
-                  <input v-model="phone" type="tel" placeholder="Please fill in your phone number?" class="form-control form-control-lg">
+                  <input v-model="form.phone" type="tel" placeholder="Please fill in your phone number?" class="form-control form-control-lg">
                   <br>
-                  <input v-model="email" type="email" placeholder="Please fill in your email?" class="form-control form-control-lg">
+                  <input v-model="form.email" type="email" placeholder="Please fill in your email?" class="form-control form-control-lg">
             </section>
             <section>
                 <h5></h5>
-                <textarea v-model="message" placeholder="Please provide the time you checked in & out"  class="form-control form-control-lg"></textarea>
+                <textarea v-model="form.message" placeholder="Please provide the time you checked in & out"  class="form-control form-control-lg"></textarea>
             </section>
             <section>
-                <input v-model="table" type="number" placeholder="Please fill in your table number?" class="form-control form-control-lg">
+                <input v-model="form.table" type="number" placeholder="Please fill in your table number?" class="form-control form-control-lg">
             </section>
-            <button class="btn-danger">submit</button>
+            <b-button class="btn-warning" @click="submitForm">submit</b-button>
+            
         </form>
     </div>
 </template>
@@ -28,14 +29,14 @@ export default {
     name: "Form",
     data() {
     return {
-      form: [{
+      form: {
                name:'',
                email: '',
                phone: '',
                message: '',
-              table: ''
+               table: ''
                }
-            ]
+            
     }
     },
     methods: {
@@ -43,19 +44,11 @@ export default {
             console.log({name: this.name, email: this.email});
             alert('Processing');
         },
-        sub: function(){
+        submitForm: function(){
+            this.$emit('SendForm',this.form);
+            this.form = '';
 
         }
-
-        
-    /*     submit(e){
-            e.preventDefault();
-            const newForm = {
-
-            }
-            this.$emit('form', newForm);
-
-        } */
     },
 }
 
